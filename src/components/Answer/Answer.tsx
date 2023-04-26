3875540e4403b2fd7d8f0aad19f6b5e52c89a99a
hcampos <hcampospro@gmail.com>
2023-04-26 14:16:38 +0200

    ✨ logic for answer component

--- src/components/Answer/Answer.tsx
+++ src/components/Answer/Answer.tsx
@@ -1,16 +1,32 @@
 //Librairies
-import React from 'react'
+import { FC } from 'react'
 
-export const Answer = () => {
+//Store
+import { useAnswerStore } from '../../hooks/useAnswer/useAnswer'
+
+type AnswerProps = {
+  answer: string
+}
+
+export const Answer: FC<AnswerProps> = ({ answer }) => {
+  const setAnswer = useAnswerStore((state) => state.setAnswer)
+
   return (
     <div className='bg-zinc-500 h-16 rounded-md'>
-      <input type='checkbox' id='answer-one' value='' className='hidden peer' />
+      <input
+        type='checkbox'
+        id={answer}
+        className='hidden peer'
+        onChange={() => {
+          setAnswer(answer)
+        }}
+      />
       <label
-        htmlFor='answer-one'
+        htmlFor={answer}
         className='inline-flex items-center justify-between rounded-md w-full h-full border-2 text-white peer-checked:border-blue-400 hover:border-gray-200 cursor-pointer peer-checked:text-blue-400'
       >
         <div className='block'>
-          <div className='w-full text-lg font-semibold px-4'>React Js</div>
+          <div className='w-full text-lg font-semibold px-4'>{answer}</div>
         </div>
       </label>
     </div>
