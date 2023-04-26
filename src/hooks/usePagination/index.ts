6052fa5f4679ec59f805ce05358e366ab89a413f
hcampos <hcampospro@gmail.com>
2023-04-26 14:18:32 +0200

    ✨ usePagination

---
+++ src/hooks/usePagination/index.ts
@@ -0,0 +1,1 @@
+export { usePaginationStore } from './usePagination'
---
+++ src/hooks/usePagination/usePagination.tsx
@@ -0,0 +1,14 @@
+//Librairies
+import { create } from 'zustand'
+
+type PaginationStore = {
+  currentPage: number
+  nextPage: () => void
+  previousPage: () => void
+}
+
+export const usePaginationStore = create<PaginationStore>((set) => ({
+  currentPage: 0,
+  nextPage: () => set((state) => ({ currentPage: state.currentPage + 1 })),
+  previousPage: () => set((state) => ({ currentPage: state.currentPage - 1 })),
+}))
