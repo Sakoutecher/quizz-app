9aa9d6690d5a55a57f5d01482156380582039673
hcampos <hcampospro@gmail.com>
2023-04-26 15:55:30 +0200

    ✨ resetPage function

--- src/hooks/usePagination/usePagination.tsx
+++ src/hooks/usePagination/usePagination.tsx
@@ -5,10 +5,12 @@ type PaginationStore = {
   currentPage: number
   nextPage: () => void
   previousPage: () => void
+  resetPage: () => void
 }
 
 export const usePaginationStore = create<PaginationStore>((set) => ({
   currentPage: 0,
   nextPage: () => set((state) => ({ currentPage: state.currentPage + 1 })),
   previousPage: () => set((state) => ({ currentPage: state.currentPage - 1 })),
+  resetPage: () => set((state) => ({ currentPage: (state.currentPage = 0) })),
 }))
