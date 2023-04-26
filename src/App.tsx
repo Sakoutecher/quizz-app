6f752fa88b708b957973f4757b9b363759334fa7
hcampos <hcampospro@gmail.com>
2023-04-26 14:25:41 +0200

    ✨ content app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
-import React, { ReactElement } from 'react'
+//Librairies
+import { ReactElement } from 'react'
+
+//Components
+import { Quizz } from '#/Quizz'
+
+//Logo
+import GitHubLogo from '../public/github-mark.svg'
 
 const App = (): ReactElement => {
-  return <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+  return (
+    <div className='w-full h-screen flex justify-center items-center flex-col bg-zinc-200 font-jetbrains'>
+      <h1 className='mb-8 text-3xl underline'>QUIZZ APP</h1>
+      <Quizz />
+      <span className='text-md fixed bottom-5 left-6 flex items-center justify-center gap-3'>
+        Star the project on{' '}
+        <a
+          href='https://github.com/Sakoutecher/quizz-app'
+          target='_blank'
+          rel='noreferrer'
+          className='py-3 px-4 bg-zinc-300 rounded-md flex items-center justify-center gap-2 hover:bg-zinc-400 transition ease-linear'
+        >
+          <img className='w-6' src={GitHubLogo} />
+          Github
+        </a>
+      </span>
+    </div>
+  )
 }
 
 export default App
