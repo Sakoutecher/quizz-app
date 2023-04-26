e90763003cc1b2ae38b5f6f8cbbce0aef9deb2f1
hcampos <hcampospro@gmail.com>
2023-04-26 14:41:58 +0200

    ♻️ use the status store

--- src/hooks/useAnswer/useAnswer.ts
+++ src/hooks/useAnswer/useAnswer.ts
@@ -4,19 +4,23 @@ import { create } from 'zustand'
 type AnswserStore = {
   answer: string
   setAnswer: (answer: string) => void
-  verifyAnswer: (trueAnswer: string, nextPage: () => void) => void
+  verifyAnswer: (
+    trueAnswer: string,
+    setStatus: (status: 'win' | 'loose') => void
+  ) => void
 }
 
 export const useAnswerStore = create<AnswserStore>((set) => ({
   answer: '',
   setAnswer: (answer) => set({ answer }),
-  verifyAnswer: (trueAnswer, nextPage) => {
+  verifyAnswer: (trueAnswer, setStatus) => {
     const { answer } = useAnswerStore.getState()
     if (answer === trueAnswer) {
       console.log('Bonne réponse')
-      nextPage()
+      setStatus('win')
     } else {
       console.log('Mauvais réponse')
+      setStatus('loose')
     }
   },
 }))
