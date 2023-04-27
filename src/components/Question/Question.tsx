82d181dc19acfb7794d770303ee9c124ea4ad5d1
hcampos <hcampospro@gmail.com>
2023-04-27 11:10:05 +0200

    💫 add question transition

--- src/components/Question/Question.tsx
+++ src/components/Question/Question.tsx
@@ -1,10 +1,21 @@
 //Librairies
 import { FC } from 'react'
+import { motion } from 'framer-motion'
 
 type QuestionProps = {
   question: string
 }
 
 export const Question: FC<QuestionProps> = ({ question }) => {
-  return <h4 className='text-xl mb-2 font-semibold'>{question}</h4>
+  return (
+    <motion.h4
+      initial={{ transform: 'translateY(-20px)', opacity: 0 }}
+      animate={{ transform: 'translateY(0px)', opacity: 1 }}
+      exit={{ transform: 'translateY(-20px)', opacity: 0 }}
+      transition={{ duration: 0.5 }}
+      className='text-xl mb-2 font-semibold'
+    >
+      {question}
+    </motion.h4>
+  )
 }
